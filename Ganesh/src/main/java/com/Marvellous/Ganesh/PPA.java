176d059a52f3a42f0425d9fa627e6af0054983b3
Sagar Gadave <sagargadave1234@gmail.com>
2025-06-18 12:32:10 +0530

    add PPA class file

---
+++ Ganesh/src/main/java/com/Marvellous/Ganesh/PPA.java
@@ -0,0 +1,14 @@
+package com.Marvellous.Ganesh;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PPA
+{
+    @GetMapping("/")
+    public String demo()
+    {
+        return "Jay Ganesh...";
+    }
+}
