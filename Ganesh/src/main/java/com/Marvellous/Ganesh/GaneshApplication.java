9cb8e4a090434aa6c79f8d978f829fa1536d38f8
Sagar Gadave <sagargadave1234@gmail.com>
2025-06-18 12:16:01 +0530

    add Ganesh project

---
+++ Ganesh/src/main/java/com/Marvellous/Ganesh/GaneshApplication.java
@@ -0,0 +1,13 @@
+package com.Marvellous.Ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaneshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaneshApplication.class, args);
+	}
+
+}
---
+++ Ganesh/src/test/java/com/Marvellous/Ganesh/GaneshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Marvellous.Ganesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaneshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
