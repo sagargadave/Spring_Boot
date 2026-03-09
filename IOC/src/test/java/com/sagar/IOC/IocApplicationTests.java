108894abbd95944fd745afcbe7933ae728fadff1
Sagar Gadave <sagargadave1234@gmail.com>
2026-03-09 17:22:21 +0530

    add  Spring Boot project demonstrating IoC

---
+++ IOC/src/test/java/com/sagar/IOC/IocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.IOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
